51e31bcfb5051a4175dc959f07d53f51d593fecf
Omar Saleh <omarsaleh92@gmail.com>
2020-09-27 20:03:51 -0500

    Fixed logic and some styling

--- script.js
+++ script.js
@@ -1,4 +1,36 @@
-var computerSelection = Math.floor(Math.random() * 3);
+/* var playerCounter = 0;
+var cpuCounter = 0;
+
+while (playerCounter < 5 && cpuCounter < 5 ) {
+    var computerSelection = Math.floor(Math.random() * 3);
+    var playerSelection = prompt("Rock, paper or scissors").toLowerCase();
+    computerPlay();
+    compare(playerSelection, computerSelection)
+}
+
+function compare (playerSelection, computerSelection) {
+if ( playerSelection == "paper" && computerSelection == "Rock") {
+    playerCounter++
+    return "Player wins!"
+} else if ( playerSelection == "paper" && computerSelection == "Scissors") {
+    cpuCounter++
+    return "CPU wins!"
+} else if ( playerSelection == "rock" && computerSelection == "Paper") {
+    cpuCounter++
+    return "CPU wins!"
+} else if ( playerSelection == "rock" && computerSelection == "Scissors") {
+    playerCounter++
+    return "Player wins!"
+} else if ( playerSelection == "scissors" && computerSelection == "Paper") {
+    playerCounter++
+    return "Player wins!"
+} else if ( playerSelection == "scissors" && computerSelection == "Rock") {
+    cpuCounter++
+    return "CPU wins!"
+} else {
+    return "Tie!"
+}
+}
 
 function computerPlay () {
     switch (computerSelection) {
@@ -13,27 +45,6 @@ function computerPlay () {
             break;
     }
     return computerSelection;
-}
+} */
 
-var playerSelection = "Paper";
 
-function compare (playerSelection, computerSelection) {
-    if ( playerSelection == "Paper" && computerSelection == "Rock") {
-        return "Player wins!"
-    } else if ( playerSelection == "Paper" && computerSelection == "Scissors") {
-        return "CPU wins!"
-    } else if ( playerSelection == "Rock" && computerSelection == "Paper") {
-        return "CPU wins!"
-    } else if ( playerSelection == "Rock" && computerSelection == "Scissors") {
-        return "Player wins!"
-    } else if ( playerSelection == "Scissors" && computerSelection == "Paper") {
-        return "Player wins!"
-    } else if ( playerSelection == "Scissors" && computerSelection == "Rock") {
-        return "CPU wins!"
-    } else {
-        return "Tie!"
-    }
-}
-
-computerPlay();
-console.log(compare(playerSelection, computerSelection));
